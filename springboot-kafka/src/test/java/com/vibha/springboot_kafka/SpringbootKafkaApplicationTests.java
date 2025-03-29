20422ce19a3d87dfe1eb3c7399e83bb97e4a442d
avi-vibha <avi.vib@protonmail.com>
2025-03-29 13:37:31 +0530

    Inital project source code

---
+++ springboot-kafka/src/test/java/com/vibha/springboot_kafka/SpringbootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibha.springboot_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
